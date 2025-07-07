3e1a39809cf16788c098b6556b3d834f999fd3e1
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2025-07-08 05:54:12 +0700

    Check solution - ProgressBar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,47 +7,39 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
-  padding: var(--margin);
-  border-radius: var(--borderRadius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  border-radius: var(--radius);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trim off corners when progress bar is near-full */
+  overflow: hidden;
 `;
 
 const Bar = styled.div`
-  width: ${(props) => `${props.$width}%`};
+  width: var(--width);
   height: var(--height);
   background-color: ${COLORS.primary};
-  border-radius: ${({ $roundRightEdgeIfFull }) =>
-    $roundRightEdgeIfFull
-      ? 'var(--borderRadius)'
-      : 'var(--borderRadius) 0 0 var(--borderRadius)'};
+  border-radius: 4px 0 0 4px;
 `;
 
-const WRAPPER_SIZES = {
+const STYLES = {
   small: {
-    '--margin': 0 + 'px',
-    '--borderRadius': 4 + 'px',
+    height: 8,
+    padding: 0,
+    radius: 4,
   },
   medium: {
-    '--margin': 0 + 'px',
-    '--borderRadius': 4 + 'px',
+    height: 12,
+    padding: 0,
+    radius: 4,
   },
   large: {
-    '--margin': 4 + 'px',
-    '--borderRadius': 8 + 'px',
-  },
-};
-
-const BAR_SIZES = {
-  small: {
-    '--height': 8 + 'px',
-    '--borderRadius': 4 + 'px',
-  },
-  medium: {
-    '--height': 12 + 'px',
-    '--borderRadius': 4 + 'px',
-  },
-  large: {
-    '--height': 24 + 'px',
-    '--borderRadius': 4 + 'px',
+    height: 16,
+    padding: 4,
+    radius: 8,
   },
 };
 
@@ -58,20 +50,31 @@ const ProgressBar = ({ value, size }) => {
     throw new Error(`Invalid progress value: ${value}`);
   }
 
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
   return (
     <Wrapper
-      style={WRAPPER_SIZES[size]}
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
       role="progressbar"
       aria-valuenow={value}
       aria-valuemin={PROGRESS_MIN}
       aria-valuemax={PROGRESS_MAX}
     >
       <VisuallyHidden>{value}%</VisuallyHidden>
-      <Bar
-        style={BAR_SIZES[size]}
-        $width={value}
-        $roundRightEdgeIfFull={value === 100}
-      />
+      <BarWrapper>
+        <Bar
+          style={{
+            '--width': value + '%',
+            '--height': styles.height + 'px',
+          }}
+        />
+      </BarWrapper>
     </Wrapper>
   );
 };
